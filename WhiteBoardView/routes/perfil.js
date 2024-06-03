0b04600b3c9335716518e7fb40d358432bb92c00
a97455 <a97455@alunos.uminho.pt>
2024-06-03 11:44:56 +0100

    views

--- WhiteBoardView/routes/perfil.js
+++ WhiteBoardView/routes/perfil.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('uc', { title: 'Express' });
+  res.render('perfil', { title: 'Express' });
 });
 
 module.exports = router;
