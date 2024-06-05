d6c91681a818dd79ff6475f15911f2bcc9649069
ema-12-martins <a97678@alunos.uminho.pt>
2024-06-05 19:42:40 +0100

    faltas put e meio

--- WhiteBoardView/routes/uc.js
+++ WhiteBoardView/routes/uc.js
@@ -38,6 +38,42 @@ router.get('/:id/aluno', function(req, res) {
     });
 });
 
+router.get('/:id/docente', function(req, res) {
+    axios.get('http://localhost:10000/ucs/' + req.params.id)
+    .then(function(response){
+        const uc = response.data;
+        if (uc != null){
+            var listaNomesProfessores = [];
+            
+            // Loop através dos docentes da UC
+            for (var i = 0; i < uc.docentes.length; i++) {
+                
+                // Faz a solicitação para obter o nome do professor
+                axios.get('http://localhost:10000/docentes/' + uc.docentes[i])
+                .then(function(responseProfessores){
+                    listaNomesProfessores.push(responseProfessores.data.nome);
+                    
+                    // Verifica se todos os nomes dos professores foram obtidos
+                    if (listaNomesProfessores.length === uc.docentes.length) {
+                        var d = new Date().toISOString().substring(0,16);
+                        res.render('informacoesDocenteUC', {uc: uc, professores: listaNomesProfessores, data: d});
+                    }
+                })
+                .catch(function(errorProfessores){
+                    console.error('Erro ao obter o nome do professor:', errorProfessores);
+                    res.render('error', {message: 'Erro ao obter o nome do professor'});
+                });
+            }
+        } else {
+            res.render('error', {message: 'Docente não registrado na WhiteBoard'});
+        }
+    })
+    .catch(function(errorUC){
+        console.error('Erro ao obter os dados da UC:', errorUC);
+        res.render('error', {message: 'Rota não existente na WhiteBoardAPI'});
+    });
+});
+
 router.get('/:id/docente/adicionarAula', function(req, res) {
   var d = new Date().toISOString().substring(0,16);
   const id = req.params.id;
@@ -74,6 +110,19 @@ router.post('/:id/docente/adicionarAula', function(req, res) {
         res.status(500).json({ error: 'Erro ao obter a UC' });
     });
 });
+
+router.get('/:id/docente/editar', function(req, res){
+    axios.get('http://localhost:10000/ucs/' + req.params.id)
+    .then(function(response) {
+        const uc = response.data;
+        var d = new Date().toISOString().substring(0,16);
+        res.render('editarUC', {uc: uc, data: d});
+    })
+    .catch(function(error) {
+        console.error('Erro ao obter UC:', error);
+        res.status(500).json({ error: 'Erro ao obter a UC' });
+    });
+})
 
 
 
