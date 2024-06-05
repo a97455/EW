fd8763085d27d7cabc045d4385fab5eab05ab2c5
a97455 <a97455@alunos.uminho.pt>
2024-06-05 17:06:10 +0100

    editar perfil, menos as fotos (altera o nome delas na bd mas não a foto no FileStore do WhiteBoardAPI

--- WhiteBoardView/routes/perfil.js
+++ WhiteBoardView/routes/perfil.js
@@ -38,6 +38,43 @@ router.get('/:id/editar', function(req, res) {
   }
 });
 
+router.post('/:id/editar', function(req, res) {
+  console.log(JSON.stringify(req.body))
+  if (req.params.id[0] == 'd'){
+    axios.put('http://localhost:10000/docentes/'+req.body._id, req.body)
+    .then(function(resposta){
+      const docente = resposta.data
+      if (docente != null){
+        res.redirect("/perfil/"+req.body._id)
+      } 
+      else{
+        res.render('error', {message: 'Docente não registado na WhiteBoard'})
+      }
+    })
+    .catch(function(){
+      res.render('error', {message: 'Rota não existente na WhiteBoardAPI'})
+    })
+  }
+  else if (req.params.id[0] == 'a'){
+    axios.put('http://localhost:10000/alunos/'+req.body._id, req.body)
+    .then(function(resposta){
+      const aluno = resposta.data
+      if (aluno != null){
+        res.redirect("/perfil/"+req.body._id)
+      } 
+      else{
+        res.render('error', {message: 'Aluno não registado na WhiteBoard'})
+      }
+    })
+    .catch(function(){
+      res.render('error', {message: 'Rota não existente na WhiteBoardAPI'})
+    })
+  }
+  else {
+    res.render('error', {message: 'Formato de ID inválido'})
+  }
+});
+
 router.get('/:id', function(req, res) {
   if (req.params.id[0] == 'd'){
     axios.get('http://localhost:10000/docentes/'+req.params.id)
