95c998dd8fbf0c9582562f2f9f784b6b868aa17d
a97455 <a97455@alunos.uminho.pt>
2024-06-09 17:24:00 +0100

    login view

--- WhiteBoardView/routes/inicio.js
+++ WhiteBoardView/routes/inicio.js
@@ -3,73 +3,45 @@ var router = express.Router();
 var axios = require('axios')
 
 router.get('/', function(req, res) {
-    var d = new Date().toISOString().substring(0,16)
     res.render('login', {title: "Autenticação"});
 })
 
+router.post('/', function(req, res) {
+    if (req.body._id[0] == 'd'){
+        res.redirect("paginaInicial/"+req.body._id)
+    }
+    else if (req.body._id[0] == 'a'){
+        res.redirect("paginaInicial/"+req.body._id)
+    }
+    else res.render('error', {message: 'Não foi possível realizar o login'});
+})
+
 router.get('/paginaInicial/:id', function(req, res) {
-    var d = new Date().toISOString().substring(0,16)
     if (req.params.id[0] == 'd'){
         axios.get('http://localhost:10000/ucs/docente/'+req.params.id)
-            .then(function(resposta){
-                axios.get('http://localhost:10000/docentes/'+req.params.id)
-                    .then(function(r){
-                        res.render('paginaInicial', {title: "Página inicial", user: r.data, lista: resposta.data, aluno: false});
-                    })
-                    .catch(function(){
-                      res.render('error', {message: 'Docente não encontrado'})
-                    })
+        .then(function(resposta){
+            axios.get('http://localhost:10000/docentes/'+req.params.id)
+            .then(function(r){
+                res.render('paginaInicial', {title: "Página inicial", user: r.data, lista: resposta.data, aluno: false});
             })
             .catch(function(){
-                res.render('error', {message: 'Não foi possível apresentar a página pretendida'})
-            }) 
+                res.render('error', {message: 'Docente não encontrado'})
+            })
+        })
+        .catch(function(){
+            res.render('error', {message: 'Não foi possível apresentar a página pretendida'})
+        }) 
     }
     else if (req.params.id[0] == 'a'){
         axios.get('http://localhost:10000/ucs/aluno/'+req.params.id)
-            .then(function(resposta){
-                res.render('paginaInicial', {title: "Página inicial", user: req.params.id, lista: resposta.data, aluno: true});
-            })
-            .catch(function(){
-                res.render('error', {message: 'Não foi possível apresentar a página pretendida'})
-            })
-        }
+        .then(function(resposta){
+            res.render('paginaInicial', {title: "Página inicial", user: req.params.id, lista: resposta.data, aluno: true});
+        })
+        .catch(function(){
+            res.render('error', {message: 'Não foi possível apresentar a página pretendida'})
+        })
+    }
     else res.render('error', {message: 'Utilizador não encontrado'});
-})
-
-
-router.post('/', function(req, res) {
-    var d = new Date().toISOString().substring(0,16)
-    if (req.body.id[0] == 'd'){
-        axios.post('http://localhost:10000/docentes/login/'+req.body.id)
-            .then(function(r){
-                axios.get('http://localhost:10000/ucs/docente/'+req.body.id)
-                    .then(function(resposta){
-                        res.render('paginaInicial', {title: "Página inicial", user: req.params.id, lista: resposta.data, aluno: false});
-                    })
-                    .catch(function(){
-                        res.render('error', {message: 'Não foi possível apresentar a página pretendida'})
-                    }) 
-            })
-            .catch(function(){
-                res.render('error', {message: 'Não foi possível realizar o login: docente não encontrado'})
-            })
-    }
-    else if (req.body.id[0] == 'a'){
-        axios.post('http://localhost:10000/alunos/login/'+req.body.id)
-            .then(function(r){
-                axios.get('http://localhost:10000/ucs/aluno/'+req.body.id)
-                    .then(function(resposta){
-                        res.render('paginaInicial', {title: "Página inicial", user: req.params.id, lista: resposta.data, aluno: true});
-                    })
-                    .catch(function(){
-                        res.render('error', {message: 'Não foi possível apresentar a página pretendida'})
-                    })
-            })
-            .catch(function(){
-                res.render('error', {message: 'Não foi possível realizar o login: Aluno não encontrado'})
-            })
-    }
-    else res.render('error', {message: 'Não foi possível realizar o login'});
 })
 
 module.exports = router;
